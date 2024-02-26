765d674a1629efb1a4ce3e36596e0238121b9190
Andrea Salazar <andrea.salazar.lizardo@gmail.com>
2024-02-26 11:58:19 -0600

    intro cutscene is finally ready !

--- Assets/Scripts/UI/script_ChangeTextAction.cs
+++ Assets/Scripts/UI/script_ChangeTextAction.cs
@@ -13,6 +13,7 @@ public class Escene
     [SerializeField] String Name;
     [SerializeField] LocalizedString key;
     [SerializeField] Sprite image;
+    
 
     public LocalizedString getdialogue()
     {
@@ -29,11 +30,25 @@ public class script_ChangeTextAction : MonoBehaviour
 {
     [SerializeField] Escene[] Escenes;
     [SerializeField] private LocalizeStringEvent localizedStringEvent;
-    [SerializeField] private Image imageComponent;       
+    [SerializeField] private Image imageComponent;
+    [SerializeField] private Color newColor;
     [SerializeField] private float AnimationTime = .8f;
     [SerializeField] private float textDelay = .8f;
+     public Animator magicAnimator;
     int i = 0;
-    
+
+    private void Start()
+    {
+        changetext();
+        Destroy(magicAnimator, 6);
+
+    }
+
+    public void colorWhite()
+    {
+        imageComponent.color = newColor;
+    }
+
     public void changetext()
     {
         if (i >= Escenes.Length - 1)
@@ -41,8 +56,10 @@ public class script_ChangeTextAction : MonoBehaviour
             //scene.ChangeRoom();
             return;
         }
+        
 
         i++;
+        
         localizedStringEvent.StringReference = Escenes[i].getdialogue();
         
         imageComponent.sprite = Escenes[i].getimage();
