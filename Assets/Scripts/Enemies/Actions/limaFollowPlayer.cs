80cd7e98bd494056f2f12c3103b73fdd9cbe3a0f
Andrea Salazar <andrea.salazar.lizardo@gmail.com>
2024-02-26 19:56:00 -0600

    lima done !

---
+++ Assets/Scripts/Enemies/Actions/limaFollowPlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class limaFollowPlayer : AIAction
+{
+
+    public NavMeshAgent enemy;
+    public Transform player;
+    public LayerMask whatisground, whatisplayer;
+    public Animator enemyAnimator;
+
+    [Header("Timer")]
+    public float timeBetweenAttacks;
+    public bool alreadyAttacked;
+
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.FindWithTag("Player").transform;
+        enemy = GetComponent<NavMeshAgent>();
+
+    }
+
+    public override void PerformAction()
+    {
+        enemy.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+
+            //enemyAnimator.SetBool("attack", true);
+            GetComponent<lima_AcidSplash>().acidSplash();
+
+            alreadyAttacked = true;
+            Invoke(nameof(resetAction), timeBetweenAttacks);
+        }
+
+
+
+    }
+
+    private void resetAction()
+    {
+        alreadyAttacked = false;
+
+    }
+
+    //enemyAnimator.SetBool("attack", false);
+
+}
