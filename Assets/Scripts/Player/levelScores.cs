6001ea20bd3fb3297a3217ff6ab7682c7efb619a
Andrea Salazar <andrea.salazar.lizardo@gmail.com>
2024-03-06 18:58:41 -0600

    coins almost done !!

--- Assets/Scripts/Player/levelScores.cs
+++ Assets/Scripts/Player/levelScores.cs
@@ -8,15 +8,28 @@ public class levelScores : MonoBehaviour
 {
     public static int levelOneScore;
     public static int levelTwoScore;
+    public static int totalPoints;
 
     public TextMeshProUGUI lvl1Score;
     public TextMeshProUGUI lvl2Score;
+    public TextMeshProUGUI totalScoreDisplay;
 
 
     private void Update()
     {
         lvl1Score.text =  levelOneScore.ToString();
         lvl2Score.text = levelTwoScore.ToString();
+
+        totalPoints = levelOneScore + levelTwoScore;
+
+        totalScoreDisplay.text = ("Coins : " + totalPoints.ToString());
+    }
+
+
+    public void buySomething(float money)
+    {
+        totalPoints -= 20;
+        Debug.Log("Bought something by " +  money.ToString()  + " coins");
     }
 
 
